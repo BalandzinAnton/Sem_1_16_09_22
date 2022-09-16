16a27681187318122a03679143e9a2b29c6c9248
Anton <balandzin2602@gmail.com>
2022-09-16 16:34:15 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sqr = Convert.ToInt32(Math.Pow(num2,2));
+if (num1 == sqr)
+{
+    Console.WriteLine("Является");
+}
+else 
+{
+    Console.WriteLine("Не является");
+}
