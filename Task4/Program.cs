cc303aee87f5f904b0b0163c14a3d32c01ce1662
Anton <balandzin2602@gmail.com>
2022-09-16 16:38:44 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = -num1;
+while (num1 >= num2)
+{
+    Console.WriteLine($"{num2}");
+    num2++;
+}
